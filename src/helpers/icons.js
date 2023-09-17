1e0167e2b832c9a1d91c14369e21d791466cd114
Ramil <garifullinramil290989@gmail.com>
2023-09-18 02:09:32 +0500

    add icons.js

---
+++ src/helpers/icons.js
@@ -0,0 +1,23 @@
+import backspace from '../img/backspace.svg';
+import division from '../img/division.svg';
+import equal from '../img/equal.svg';
+import minus from '../img/minus.svg';
+import multiplication from '../img/multiplication.svg';
+import percent from '../img/percent.svg';
+import plusMinus from '../img/plus-minus.svg';
+import plus from '../img/plus.svg';
+import reset from '../img/reset.svg';
+
+const icons = {
+  backspace,
+  division,
+  equal,
+  minus,
+  multiplication,
+  percent,
+  plusMinus,
+  plus,
+  reset,
+};
+
+export default icons;
