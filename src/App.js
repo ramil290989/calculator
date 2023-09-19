3b6ca7a18f9431ae103bdbc16209cd33830e3317
Ramil <garifullinramil290989@gmail.com>
2023-09-20 02:01:49 +0500

    add context providers

--- src/App.js
+++ src/App.js
@@ -2,23 +2,27 @@ import React, { useState } from 'react';
 import NumAContext from './context/NumA.jsx';
 import NumBContext from './context/NumB.jsx';
 import OperationContext from './context/Operation.jsx';
+import ResultContext from './context/Result.jsx';
 import './App.css';
 import Display from './components/Display.jsx';
 import ResultDisplay from './components/ResultDisplay.jsx';
 import Buttons from './components/Buttons.jsx';
 
 function App() {
-  const [numA, setNumA] = useState();
-  const [numB, setNumB] = useState();
-  const [operation, setOperation] = useState();
+  const [numA, setNumA] = useState('');
+  const [numB, setNumB] = useState('');
+  const [operation, setOperation] = useState('');
+  const [result, setResult] = useState('');
   return (
     <div className="container">
       <NumAContext.Provider value={{ numA, setNumA }}>
         <NumBContext.Provider value={{ numB, setNumB }}>
           <OperationContext.Provider value={{ operation, setOperation }}>
-            <Display />
-            <ResultDisplay />
-            <Buttons />
+            <ResultContext.Provider value={{ result, setResult }}>
+              <Display />
+              <ResultDisplay />
+              <Buttons />
+            </ResultContext.Provider>
           </OperationContext.Provider>
         </NumBContext.Provider>
       </NumAContext.Provider>
